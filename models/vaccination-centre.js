642e8517fcdae372e1dd49c60b79403a98e24c63
Vishal Kumar Paswan <kumarpaswanvishal@gmail.com>
2023-04-23 13:29:45 +0530

    Added VaccinationCentre Schema

---
+++ models/vaccination-centre.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+// Defining Vaccination Centre schema
+const vaccinationCentreSchema = new mongoose.Schema({
+    centre_name: {
+        type: String,
+        required: true,
+        maxLength: 32,
+        trim: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    phone: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    address: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    pin_code: {
+        type: Number,
+        required: true,
+        trim: true,
+        maxLength: 6
+    },
+    district: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    state: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    vaccines: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "VaccineStock"
+    },
+    available_slots: {
+        type: [Date]
+    },
+    paid: {
+        type: Boolean,
+        default: false
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model("VaccinationCentre", vaccinationCentreSchema);
